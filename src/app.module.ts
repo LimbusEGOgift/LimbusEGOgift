708919027fa2de9006d6ed18ec4557bede61cd40
LimbusEGOgift <limbusegogift@gmail.com>
2025-11-12 23:05:28 +0900

    identity api 중간 저장7

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
-import { IdentityModule } from './identity/identity.module';
+// import { IdentityModule } from './identity/identity.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HealthModule, IdentityModule],
+  imports: [ConfigModule.forRoot(), HealthModule, /*IdentityModule*/],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/identity/identity.service.ts
+++ src/identity/identity.service.ts
@@ -1,41 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { readdir, readFile } from 'node:fs/promises';
-import { basename, join } from 'node:path';
-import { FindEGOGiftDto } from './dto/findEGOGift.dto';
+// import { Injectable } from '@nestjs/common';
+// import { readdir, readFile } from 'node:fs/promises';
+// import { basename, join } from 'node:path';
+// import { FindEGOGiftDto } from './dto/findEGOGift.dto';
 
-type KeyMap = Record<string, string[]>;
+// type KeyMap = Record<string, string[]>;
 
-@Injectable()
-export class IdentityService {
-  // // 모든 인격 리스트 반환
-  // async findIdentityList(): Promise<KeyMap> {
-  //   const result: KeyMap = {};
+// @Injectable()
+// export class IdentityService {
+//   // 모든 인격 리스트 반환
+//   async findIdentityList(): Promise<KeyMap> {
+//     const result: KeyMap = {};
 
-  //   const entries = await readdir('identity', { withFileTypes: true });
-  //   await Promise.all(
-  //     entries.map(async (ent) => {
-  //       const p = join('identity', ent.name);
-  //       const Sinner = await readFile(p, 'utf-8');
-  //       const parsed = JSON.parse(Sinner);
-  //       const Identitys = Object.keys(parsed);
-  //       result[basename(ent.name, '.json')] = Identitys;
-  //     }),
-  //   );
+//     const entries = await readdir('identity', { withFileTypes: true });
+//     await Promise.all(
+//       entries.map(async (ent) => {
+//         const p = join('identity', ent.name);
+//         const Sinner = await readFile(p, 'utf-8');
+//         const parsed = JSON.parse(Sinner);
+//         const Identitys = Object.keys(parsed);
+//         result[basename(ent.name, '.json')] = Identitys;
+//       }),
+//     );
 
-  //   return result;
-  // }
+//     return result;
+//   }
 
-  // // 특정 인격이 적용받을 수 있는 EGOGift 반환
-  // async findEGOGiftForIdentity(dto: FindEGOGiftDto): Promise<KeyMap> {
-  //   // const result: KeyMap = {};
+//   // 특정 인격이 적용받을 수 있는 EGOGift 반환
+//   async findEGOGiftForIdentity(dto: FindEGOGiftDto): Promise<KeyMap> {
+//     // const result: KeyMap = {};
 
-  //   // 인격의 키워드를 가져옴
-  //   const Sinner: string = await readFile(
-  //     `identity/${dto.sinner}.json`,
-  //     'utf-8',
-  //   );
-  //   const keyWord: KeyMap = JSON.parse(Sinner)[`${dto.identity}`]['키워드'];
+//     // 인격의 키워드를 가져옴
+//     const Sinner: string = await readFile(
+//       `identity/${dto.sinner}.json`,
+//       'utf-8',
+//     );
+//     const keyWord: KeyMap = JSON.parse(Sinner)[`${dto.identity}`]['키워드'];
 
-  //   return keyWord;
-  // }
-}
+//     return keyWord;
+//   }
+// }
