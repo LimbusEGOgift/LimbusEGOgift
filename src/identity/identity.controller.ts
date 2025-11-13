2461d90bbe865f8ec6f60c91dea7b13846b788ad
LimbusEGOgift <limbusegogift@gmail.com>
2025-11-13 22:52:28 +0900

    identity api

--- src/identity/identity.controller.ts
+++ src/identity/identity.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IdentityService } from './identity.service';
+import { FindEGOGiftDto } from './dto/findEGOGift.dto';
 
 @Controller({ path: 'identity' })
 export class IdentityController {
@@ -8,5 +9,10 @@ export class IdentityController {
   @Get()
   findIdentity() {
     return this.app.findIdentityList();
+  }
+
+  @Post()
+  findEGOGift(@Body() dto: FindEGOGiftDto) {
+    return this.app.findEGOGiftForIdentity(dto);
   }
 }
--- src/identity/identity.service.ts
+++ src/identity/identity.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JsonLoaderService } from 'src/utils/json-loader.service';
 import * as path from 'path';
-// import { FindEGOGiftDto } from './dto/findEGOGift.dto';
+import { FindEGOGiftDto } from './dto/findEGOGift.dto';
 
 type KeyMap = Record<string, string[]>;
 
@@ -16,13 +16,33 @@ export class IdentityService {
     const allNames: KeyMap = {};
     for (const sinner of Object.keys(files)) {
       for (const data of Object.values(files)) {
-        const identities = Object.keys(data);
-        allNames[path.basename(sinner, '.json')] = identities;
+        const identity = Object.keys(data);
+        allNames[path.basename(sinner, '.json')] = identity;
       }
     }
 
     return allNames;
   }
 
-  // async findEGOGiftForIdentity(dto: FindEGOGiftDto) {}
+  async findEGOGiftForIdentity(dto: FindEGOGiftDto): Promise<Record<string, KeyMap>> {
+    const sinnerDir = path.resolve(`identity/${dto.sinner}.json`);
+    const identity = await this.jsonLoader.readSingleJson<Record<string, any>>(sinnerDir);
+    const keyWords = new Set(identity[dto.identity]["키워드"]);
+
+    const EGOGiftDir = path.resolve('EGOGift');
+    const files = await this.jsonLoader.readJsonFiles<Record<string, any>>(EGOGiftDir);
+
+    const allNames: Record<string, KeyMap> = {};
+    for(const category of Object.keys(files)){
+      for(const EGOGift of Object.values(files)){
+        for(const data of Object.values(EGOGift)){
+          const condition: string[] = data["조건"];
+          const check = condition.filter(x => keyWords.has(x));
+          if(check.length) allNames[path.basename(category, '.json')] = EGOGift;
+        }
+      }
+    }
+
+    return allNames;
+  }
 }
